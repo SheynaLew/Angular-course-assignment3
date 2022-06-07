757965e702bebafdc94ac510e89855b1e54b13d8
Jenny Lewis-Gandy <jennylewis-gandy@Jennys-MacBook-Pro.local>
2022-06-07 08:52:24 +0100

    Task 3 done, paragraph now toggles on and off

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayParagraph=false;
+
+  onDisplayParagraph() {
+    if (this.displayParagraph === false) {
+      this.displayParagraph = true;
+    } else {
+      this.displayParagraph = false;
+    }
+    console.log(this.displayParagraph)
+  }
 }
